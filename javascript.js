77c510cc25cd11db6f090d463c2241bdd50c1623
Craser <101993553+Craser737@users.noreply.github.com>
2024-01-31 22:03:33 -0800

    build loop function for 5 games and print result

--- javascript.js
+++ javascript.js
@@ -1,30 +1,14 @@
 // SET Variable for computerSelection and CALL function getComputerChoice
-let computerSelection = getComputerChoice();
-console.log("computer chooses " + computerSelection + "!");
-
-// GET input for player selection, SET to lower case, ASSIGN to variable playerSelection
-let playerSelection = prompt("Rock, scissors, or paper?").toLowerCase();
-
-// DETERMINE input is valid or prompt for re-entry
-while (playerSelection !== "rock" && playerSelection !== "scissors" && playerSelection !== "paper") {
-   playerSelection = prompt("Invalid entry. Please try rock, scissors, or paper?").toLowerCase();
-};
-console.log("Player chooses " + playerSelection + "!");
 
 // win <- 0, lose <- 0 rounds <- 0
 let win = 0, lose = 0, rounds = 0;
 
-// Create function for 5 round game
+playGame();
 
-// CALL function playRound and SET as variable roundResult
-let roundResult = playRound(playerSelection, computerSelection);
-console.log(roundResult);
+
 // // COMPUTE variable for computerSelection with function getComputerChoice 
 function getComputerChoice() {
-//   COMPUTE random number between 1 and 3
   let hand = Math.floor(Math.random() * 3) +1;
-
-  //   IF 1 THEN "rock", ELSE IF 2 THEN "scissors" ELSE "paper"
   if (hand === 1) {
     return "rock";
   } else if (hand === 2) {
@@ -34,23 +18,61 @@ function getComputerChoice() {
 };
 
 // COMPARE playerSelection to computerSelection
+// increment win, lose, rounds as appropriate 
+// DISPLAY a win/lose message for each combination
 function playRound(playerSelection, computerSelection) {
-//   DISPLAY a win/lose message for each combination
-if (playerSelection === "rock" && computerSelection === "scissors") {
-  console.log("Rock beats scissors You win!")
-} else if (playerSelection === "rock" && computerSelection === "paper") {
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    win = ++win, rounds = ++rounds;
+    return ("Rock beats scissors You win!")
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    lose = ++lose, rounds = ++rounds;
     return "Paper beats rock. You lose!";
-} else if (playerSelection === "scissors" && computerSelection === "rock") {
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    lose = ++lose, rounds = ++rounds;
     return "Rock beats scissors You Lose!";
-} else if (playerSelection === "scissors" && computerSelection === "paper") {
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    win = ++win, rounds = ++rounds;
     return "Scissors beat paper. You win!";
-} else if (playerSelection === "paper" && computerSelection === "rock") {
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    win = ++win, rounds = ++rounds;
     return "Paper beats rock. You win!";
-} else if (playerSelection === "paper" && computerSelection === "scissors") {
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    lose = ++lose, rounds = ++rounds;
     return "Scissors beat paper. You lose!";
-} else {
-    return "Tie. Please try again"};
+  } else {
+    rounds = ++rounds;
+    return "Tie. Please try again."
+  };
 };
+
+// Create function playGame for 5 rounds game
+function playGame() {
+  while (rounds < 5) {
+    let computerSelection = getComputerChoice();
+    console.log("computer chooses " + computerSelection + "!");
+
+// GET input for player selection, SET to lower case, ASSIGN to variable playerSelection
+    let playerSelection = prompt("Rock, scissors, or paper?").toLowerCase();
+
+// DETERMINE input is valid or prompt for re-entry
+    while (playerSelection !== "rock" && playerSelection !== "scissors" && playerSelection !== "paper") {
+    playerSelection = prompt("Invalid entry. Please try rock, scissors, or paper?").toLowerCase();
+    };
+    console.log("Player chooses " + playerSelection + "!");
+
+    // CALL function playRound and SET as variable roundResult
+    let roundResult = playRound(playerSelection, computerSelection);
+    console.log(roundResult);
+};
+
+    if (win > lose) {
+    console.log("Congratulations, you win!");
+  } else if (win < lose) {
+    console.log("You lost. Better luck next time!");
+  } else {
+    console.log("Tie game! wha-waaa...");
+  };
+  }
 
 
 // playRound function INCREMENTS win or lose, and rounds
