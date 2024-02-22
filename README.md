4c4b57968c49dd1c1b787baabf7aaaa7d7d80942
Craser <101993553+Craser737@users.noreply.github.com>
2024-02-22 14:20:09 -0800

    Add UI with buttons and results displayed on page

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
 Foundations program project based on the above resource.
 Build game using JavaScript that compairs "hands" and prints the result after 5 games.
-User is prompted for input (rock, scissors, or paper) and will be re-prompted if input is invalid.
-Results are displayed in the browser console using console.log function.
+User selects button of choice and the results are printed below. After 5 rounds the final winner is announced. On additional "click" (6th) results are reset.
--- javascript.js
+++ javascript.js
@@ -1,7 +1,32 @@
-// win <- 0, lose <- 0 rounds <- 0
-let win = 0, lose = 0, rounds = 0;
+const body = document.querySelector('body');
+const rockBtn = document.querySelector('#rock');
+const scissorsBtn = document.querySelector('#scissors');
+const paperBtn = document.querySelector('#paper');
+const results = document.querySelector('#round');
+const player = document.querySelector('#player');
+const computer = document.querySelector('#computer');
+const final = document.querySelector('#final');
+ 
 
-playGame();
+body.addEventListener('click', (e) => {
+  const target = e.target
+  let playerSelection = '';
+  console.log(playerSelection);
+  switch (target) {
+    case rockBtn:
+      playerSelection = 'rock';
+      break;
+    case scissorsBtn:
+      playerSelection = 'scissors';
+      break;
+    case paperBtn:
+      playerSelection = 'paper';
+      break;
+    }
+  playRound(playerSelection);
+});
+
+let win = 0, lose = 0, rounds = 0;
 
 
 // // COMPUTE variable for computerSelection with function getComputerChoice 
@@ -12,60 +37,59 @@ function getComputerChoice() {
   } else if (hand === 2) {
         return "scissors";
   } else {
-        return "paper"; }
-};
-
-// COMPARE playerSelection to computerSelection and output results for 1 round
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === "rock" && computerSelection === "scissors") {
-    win = ++win, rounds = ++rounds;
-    return ("Rock beats scissors You win!")
-  } else if (playerSelection === "rock" && computerSelection === "paper") {
-    lose = ++lose, rounds = ++rounds;
-    return "Paper beats rock. You lose!";
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    lose = ++lose, rounds = ++rounds;
-    return "Rock beats scissors You Lose!";
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    win = ++win, rounds = ++rounds;
-    return "Scissors beat paper. You win!";
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    win = ++win, rounds = ++rounds;
-    return "Paper beats rock. You win!";
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    lose = ++lose, rounds = ++rounds;
-    return "Scissors beat paper. You lose!";
-  } else {
-    rounds = ++rounds;
-    return "Tie. Please try again."
-  };
-};
-
-// Create function playGame for 5 rounds game
-function playGame() {
-  while (rounds < 5) {
-    let computerSelection = getComputerChoice();
-    console.log("computer chooses " + computerSelection + "!");
-
-    // GET valid player selection
-    let playerSelection = prompt("Rock, scissors, or paper?").toLowerCase();
-    while (playerSelection !== "rock" && playerSelection !== "scissors" && playerSelection !== "paper") {
-    playerSelection = prompt("Invalid entry. Please try rock, scissors, or paper?").toLowerCase();
-    };
-    console.log("Player chooses " + playerSelection + "!");
-
-    // CALL function playRound 
-    let roundResult = playRound(playerSelection, computerSelection);
-    console.log(roundResult);
-};
-
-  if (win > lose) {
-    console.log("Congratulations, you win!");
-  } else if (win < lose) {
-    console.log("You lost. Better luck next time!");
-  } else {
-    console.log("Tie game! wha-waaa...");
-  };
+        return "paper";
   }
+}
 
+function playRound(playerSelection) {
+  if (rounds === 0) {
+    final.textContent = "";  
+    results.textContent = '';
+  }
+  let computerSelection = getComputerChoice(); 
+  const li = document.createElement('li');
+  
+  player.textContent = ('player chooses ' + playerSelection + '!');
+  computer.textContent = ("computer chooses " + computerSelection + '!');  
+    
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    li.textContent = "Rock beats scissors You win!";
+    results.appendChild(li);
+    win++;
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    li.textContent = "Paper beats rock. You lose!";
+    results.appendChild(li);
+    lose++; 
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    li.textContent = "Rock beats scissors You Lose!";
+    results.appendChild(li);
+    lose++; 
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    li.textContent = "Scissors beat paper. You win!";
+    results.appendChild(li);
+    win++;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    li.textContent = "Paper beats rock. You win";
+    results.appendChild(li);
+    win++;
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    li.textContent = "Scissors beat paper. You lose!";
+    results.appendChild(li);
+    lose++; 
+  } else {
+    li.textContent = "Tie. Please try again.";
+    results.appendChild(li);
+  };
+  rounds++;
 
+  if (rounds === 5) {
+    if (win > lose) {
+      final.textContent = "Congratulations, you win!";
+    } else if (win < lose) {
+      final.textContent = "You lost. Better luck next time!";
+    } else {
+      final.textContent = "Tie game! wha-waaa...";
+    }
+    rounds = 0;
+  }
+}
